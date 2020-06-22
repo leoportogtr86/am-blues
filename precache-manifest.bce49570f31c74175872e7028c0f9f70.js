94c66d81e6c3d67b22c0776b9621ce826f8e81c6
Leo Porto <leonardoreisporto@gmail.com>
2020-06-22 06:19:53 -0300

    Updates

---
+++ precache-manifest.bce49570f31c74175872e7028c0f9f70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "188341bf4c04e6fab8b3143e6d679fa7",
+    "url": "/am-blues/index.html"
+  },
+  {
+    "revision": "713e0e45f121ddb18936",
+    "url": "/am-blues/static/css/main.6db28dbe.chunk.css"
+  },
+  {
+    "revision": "fbe05bdb81e5662ec041",
+    "url": "/am-blues/static/js/2.dc5b70c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/am-blues/static/js/2.dc5b70c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "713e0e45f121ddb18936",
+    "url": "/am-blues/static/js/main.5acaba56.chunk.js"
+  },
+  {
+    "revision": "087b9013456050cfaae4",
+    "url": "/am-blues/static/js/runtime-main.f99ad799.js"
+  }
+]);
